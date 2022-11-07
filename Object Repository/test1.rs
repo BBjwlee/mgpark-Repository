96b622731f1a77add5438af25afbe588c31cca74
BBjwlee <llljjj123@bbros.kr>
2022-11-07 16:21:40 +0900

    testjw

---
+++ Object Repository/test1.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test1</name>
+   <tag></tag>
+   <elementGuidId>65e460ba-a566-437e-9f08-869d87d732f4</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/test2.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test2</name>
+   <tag></tag>
+   <elementGuidId>9a480517-048d-4132-a87e-5804905ffd0b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
